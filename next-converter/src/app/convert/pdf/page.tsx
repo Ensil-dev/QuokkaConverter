e7d1719fa7614b995f762b42c761943594978527
Ensil <lasags@naver.com>
2025-07-03 15:31:56 +0900

    fix: 중복 로그인 인증 로직 제거

--- next-converter/src/app/convert/pdf/page.tsx
+++ next-converter/src/app/convert/pdf/page.tsx
@@ -1,7 +1,5 @@
 import PdfConverter from '@/components/PdfConverter';
-import requireAuth from '@/lib/requireAuth';
 
 export default async function PdfConvertPage() {
-  await requireAuth();
   return <PdfConverter />;
 }
