68fd46a68b5b484f553d8c150d000d1f6874e9d6
Ensil <lasags@naver.com>
2025-07-06 00:03:13 +0900

    feature: 변환 준비 완료 화면 추가

--- next-converter/src/components/PdfConverter.tsx
+++ next-converter/src/components/PdfConverter.tsx
@@ -150,7 +150,7 @@ export default function PdfConverter() {
         />
       )}
 
-      {files && !loading && !result && (
+      {files && !loading && !result && !error && (
         <ResultPlaceholder
           ready
           icon="📁"
