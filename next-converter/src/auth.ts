525a822fc1049fadc943ebf5f843a1463ddab33a
Ensil-dev <lasags@naver.com>
2025-08-29 15:58:51 +0900

    fix

--- next-converter/src/auth.ts
+++ next-converter/src/auth.ts
@@ -1,6 +1,10 @@
 import NextAuth from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 
+if (process.env.NODE_ENV === "development") {
+  process.env.NEXTAUTH_URL = "http://localhost:3000";
+}
+
 export const { handlers, auth, signIn, signOut } = NextAuth({
   providers: [
     GoogleProvider({
@@ -28,4 +32,5 @@ export const { handlers, auth, signIn, signOut } = NextAuth({
     strategy: "jwt",
   },
   secret: process.env.NEXTAUTH_SECRET,
-}); 
+  trustHost: true,
+});
